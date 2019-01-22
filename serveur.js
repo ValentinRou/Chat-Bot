d7aac6302e1a60a717a9f0a29bc2bb2d5bda6901
valentin <valentin.rouviere@eemi.com>
2019-01-22 15:57:04 +0100

    added a procfiles.

--- serveur.js
+++ serveur.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/Hello', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World')
 })
 
 app.listen(3000, function () {
-  console.log('Hello World!')
+  console.log('Hello World')
 })
