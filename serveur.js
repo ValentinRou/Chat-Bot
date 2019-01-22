64c819d24841fa8412bb2dde36bb95182855d459
valentin <valentin.rouviere@eemi.com>
2019-01-22 15:43:01 +0100

    server.js update

--- serveur.js
+++ serveur.js
@@ -1,3 +1,10 @@
+const express = require('express')
+const app = express()
 
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-console.log("Hello World") 
+app.listen(3000, function () {
+  console.log('Hello World!')
+})
