65b07dbdcac80713fb016b73d02cafe7053ac0d3
valentin <valentin.rouviere@eemi.com>
2019-02-08 16:21:54 +0100

    modif serveur.js

--- serveur.js
+++ serveur.js
@@ -13,13 +13,18 @@ app.get('/Hello', function (req, res) {
 app.post('/chat', function (req, res) {
   if (req.body.msg === "ville"){
     res.send("Nous sommes à Paris");
-    
   }else if (req.body.msg === "meteo"){
     res.send("Il fait beau");
-  }else {
+  }else if (req.body.msg === "demain"){
+    res.send("Je ne connais pas demain…");
+  }else if (req.body.msg === "demain = Mercredi"){
+    res.send("Merci pour cette information !");
+  }
+  else {
     res.send("commande inconnue");
   }
 })
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Hello World')
